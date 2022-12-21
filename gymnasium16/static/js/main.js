3289907c23abfc2192ede5d66680d83603a7cb81
Xedll <97981835+Xedll@users.noreply.github.com>
2022-12-21 21:11:37 +0300

    help me pls
    im very tired

--- gymnasium16/static/js/main.js
+++ gymnasium16/static/js/main.js
@@ -1,33 +1,28 @@
 // @ts-nocheck
 
-//Анимации появления
-ScrollReveal().reveal('.news__item:nth-of-type(2)', { delay: 200, distance: '20px', origin: 'top' })
-ScrollReveal().reveal('.news__item', { distance: '20px', origin: 'top' })
-ScrollReveal().reveal('.news__item:nth-child(3n)', { delay: 400, distance: '20px', origin: 'top' })
-ScrollReveal().reveal('.news__item:nth-of-type(5)', { delay: 200, distance: '20px', origin: 'top' })
-ScrollReveal().reveal('.header__content', { distance: '10px', origin: 'top' })
-ScrollReveal().reveal('.home__title', { delay: 100, distance: '10px', origin: 'left' })
-ScrollReveal().reveal('.home__descriprion', { delay: 200, distance: '10px', origin: 'right' })
-ScrollReveal().reveal('.home__ask', { delay: 300, origin: 'bottom' })
-ScrollReveal().reveal('.achivements__title', { distance: '10px', origin: 'top' })
-ScrollReveal().reveal('.achivements__item:nth-of-type(2)', { delay: 200, distance: '20px', origin: 'top' })
-ScrollReveal().reveal('.achivements__item', { distance: '20px', origin: 'top' })
-ScrollReveal().reveal('.achivements__item:nth-child(3n)', { delay: 400, distance: '20px', origin: 'top' })
-ScrollReveal().reveal('.achivements__item:nth-of-type(5)', { delay: 200, distance: '20px', origin: 'top' })
-ScrollReveal().reveal('.about__title', { distance: '20px', origin: 'top' })
-ScrollReveal().reveal('.about__content', { delay: 100, distance: '20px', origin: 'left' })
-ScrollReveal().reveal('.contact__content', { delay: 200, distance: '20px', origin: 'right' })
-
-
 //Прокрутка
 document.addEventListener('click', (event) => {
    if (event.target.dataset.anchor) {
-      console.log(1)
       event.preventDefault()
       document.querySelector(`#${event.target.dataset.anchor}`).scrollIntoView({ behavior: 'smooth' })
    }
 })
 
-window.onload = () => {
-   console.log(1)
+//Появление блоков
+function onEntry(entry) {
+   entry.forEach(change => {
+      if (change.isIntersecting) {
+         change.target.classList.add('element-show');
+      }
+   });
+}
+
+let options = {
+   threshold: [0.5]
+};
+let observer = new IntersectionObserver(onEntry, options);
+let elements = document.querySelectorAll('.element-animation');
+
+for (let elm of elements) {
+   observer.observe(elm);
 }
